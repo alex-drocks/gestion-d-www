31fddd88e28be88bc55d64af4ae9008ce8731f76
alex-drocks <alexandre_d_7@hotmail.com>
2020-11-30 16:02:14 -0500

    attempt #2 to Fix banner width after adding new route which page title was taking 3 text rows.

--- src/routes/services/impots-societes/ImpotsSocietes.jsx
+++ src/routes/services/impots-societes/ImpotsSocietes.jsx
@@ -13,7 +13,13 @@ export default function ImpotsSocietes({
     <div className="route-container container">
       <Banner pageTitle={pageTitle} pageDescription={pageDescription} />
 
-      <div className="content-block">
+      <div className="content-block spacer-top force-flex-align-center">
+        <div className="flex-col flex-col--40w">
+          <p>
+            Au Québec, les entreprises incorporées doivent produire une déclaration d'impôt T2 au fédéral ainsi qu'une
+            déclaration d'impôt C0-17 au provincial, et ce, même si elles ne doivent pas payer d'impôts.
+          </p>
+        </div>
 
       </div>
     </div>
