79bb7207ca73260455ca27331ba5822734d5b661
Alexandre Desroches <alex.desroches7@gmail.com>
2021-02-28 07:26:50 -0500

    Update submit-contact-form.php to use env variables and have proper Headers.

--- public/api/submit-contact-form.php
+++ public/api/submit-contact-form.php
@@ -7,44 +7,44 @@ echo (function () {
 
 
     // form person's name
-    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $name = urldecode($name);
-    if (!$name) {
+    $client_name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $client_name = urldecode($client_name);
+    if (!$client_name) {
         return "Error: name empty";
     }
 
 
     // form person's email address
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-    $email = urldecode($email);
-    if (!$email) {
+    $client_email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $client_email = urldecode($client_email);
+    if (!$client_email) {
         return "Error: email empty after sanitizing";
     }
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($client_email, FILTER_VALIDATE_EMAIL)) {
         return "Error: email format failed";
     }
-    if (!domainExists($email)) {
-        return "Error: email domain " . $email . " does not exist.";
+    if (!domainExists($client_email)) {
+        return "Error: email domain " . $client_email . " does not exist.";
     }
 
 
     // form person's phone number
-    $phone = filter_input(INPUT_POST, "phone", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $phone = urldecode($phone);
+    $client_phone = filter_input(INPUT_POST, "phone", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $client_phone = urldecode($client_phone);
 
 
     // form person's subject
-    $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $subject = urldecode($subject);
-    if (!$subject) {
+    $client_subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $client_subject = urldecode($client_subject);
+    if (!$client_subject) {
         return "Error: subject empty";
     }
 
 
     // form person's message
-    $message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $message = urldecode($message);
-    if (!$message) {
+    $client_message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $client_message = urldecode($client_message);
+    if (!$client_message) {
         return "Error: message empty";
     }
 
@@ -52,29 +52,41 @@ echo (function () {
     $htmlMsg = '<html lang="fr-CA">
             <body>
               <h1>Formulaire de contact gestiondesroches.com</h1>
+              <p>Vous avez reçu un message via le formulaire de contact https://gestiondesroches.com/contact/</p>
               <br>
-              <h3>Nom: ' . $name . '</h3>
-              <p>Courriel: ' . $email . '</p>
-              <p>Téléphone: ' . $phone . '</p>
-              <p>Sujet: ' . $subject . '</p>
-              <h3>Message: ' . $message . '</h3>
+              <h3>Nom: ' . $client_name . '</h3>
+              <p>Courriel: ' . $client_email . '</p>
+              <p>Téléphone: ' . $client_phone . '</p>
+              <p>Sujet: ' . $client_subject . '</p>
+              <h3>Message: ' . $client_message . '</h3>
+              <br>
+              <br>
+              <pre>Note : Il est possible de répondre directement dans ce message.</pre>
             </body>
             </html>';
 
-    // To send HTML messages, the Content-type header must be set:
+
+    //Get send to address from env variable
+    $send_to_email_address = getenv("CONTACT_FORM_SEND_TO_EMAIL");
+    //Get CC address from env variable
+    $cc_email_address = getenv("CONTACT_FORM_CC_EMAIL");
+
+
+    //Assign proper Headers for this email
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+    $headers .= "From: formulaire@gestiondesroches.com" . "\r\n";
+    $headers .= "Reply-To: " . $client_email . "\r\n";
+    $headers .= "CC: " . $cc_email_address . "\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
 
-    // define sender address for the default php sendmail method otherwise it's financed@rockland.dns...
-    $fdSendingAdress = '-f formulaire-contact-site-internet@gestiondesroches.com';
 
-    if (mail("daniel@gestiondesroches.com",
-        "GestionDesroches.com - Formulaire de contact",
-        $htmlMsg, $headers, $fdSendingAdress)
-    ) {
+    //Send the email and return response
+    if (mail($send_to_email_address, "GestionDesroches.com — Formulaire de contact", $htmlMsg, $headers)) {
         return "EMAIL_SENT_SUCCESSFULLY";
+    } else {
+        return "ERROR";
     }
-    return "ERROR";
 
 })();
 
